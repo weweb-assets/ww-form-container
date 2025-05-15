33b89b72e7b5f0f77ef01c76ca5ad066460d9a27
Léo Coletta <mail.leo.coletta@gmail.com>
2025-05-15 12:25:45 +0200

    feat(WW-3531): add submit method to useForm composable

--- src/shared/useForm.js
+++ src/shared/useForm.js
@@ -60,6 +60,7 @@ export function useForm(
     const registerFormInput = inject('_wwForm:registerInput', () => {});
     const unregisterFormInput = inject('_wwForm:unregisterInput', () => {});
     const updateFormInput = inject('_wwForm:updateInput', () => {});
+    const submitForm = inject('_wwForm:submit', () => {});
 
     const { uid } = elementState;
     const _fieldName = computed(() => fieldName?.value || elementState.name);
@@ -217,6 +218,7 @@ export function useForm(
 
     return {
         selectForm,
+        submitForm,
     };
 }
 
